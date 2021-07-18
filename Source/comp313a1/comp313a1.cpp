968489d12cb18770c3187d32da8710217ee24d76
robotdognz <robotdognz@gmail.com>
2021-07-19 10:55:48 +1200

    Revert "Initial unreal project commit"
    This reverts commit 8a9bbe490b03f2a959f97b565eea11e2ba8d4698.

--- Source/comp313a1/comp313a1.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "comp313a1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, comp313a1, "comp313a1" );
- 
